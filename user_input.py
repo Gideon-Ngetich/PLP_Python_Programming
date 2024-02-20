754c8f6c40968f17128969d97bb54797e552e1c1
Gideon <gideon@localhost.localdomain>
2024-02-20 10:31:28 +0300

    Python user input

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = int(input("Enter you age: "))
+location = input("Enter your current location: ")
+
+print('Hello!, your name is {}, you are {} years old and currently live at {}'.format(name,age,location))
